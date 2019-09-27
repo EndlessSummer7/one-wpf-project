fbf353083078fbf33b416f0d7a03b35c52b61d70
Nastya <ashatokhina@mail.ru>
2019-09-27 21:34:17 +0300

    first commit

---
+++ CocomoProject/Calculation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocomoProject
+{
+    //public enum ProjectType
+    //{
+    //    Organic = 0, Semi_detached = 1, Embedded = 2
+    //}
+
+    public static class Calculation
+    {
+        //Таблиця коефіцієнтів
+        static double[][] coefArr = new double[3][];
+        static Calculation()
+        {
+            coefArr[0] = new[] { 2.4, 1.05, 2.5, 0.38 }; // для проекта типа Organic
+            coefArr[1] = new[] { 3.0, 1.12, 2.5, 0.35 }; // Semi-Detached
+            coefArr[2] = new[] { 3.6, 1.20, 2.5, 0.32 }; // Embedded
+        }
+
+        //Трудоємність
+        public static double GetEfforts(int codeSize, int type)
+        {
+            return coefArr[type][0] * (Math.Pow(codeSize, coefArr[type][1]));
+        }
+
+        // Термін розробки
+        public static double GetTimeToDevelop(int codeSize, int type)
+        {
+            return coefArr[type][2] * (Math.Pow(GetEfforts(codeSize, type), coefArr[type][3]));
+        }
+
+        //Число необхідних розробників
+        public static double GetPersonsToDevelop(int codeSize, int type)
+        {
+            return Math.Round(GetEfforts(codeSize, type) / GetTimeToDevelop(codeSize, type));
+        }
+    }
+
+
+
+}
+
+
---
+++ CocomoProject/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CocomoProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        bool isDigit = true;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void GetInfoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtCodeSize.Text))
+            {
+                MessageBox.Show("Вы не ввели кол-во строк кода!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            int type = comboBoxProjectType.SelectedIndex; // определяем тип проекта
+            int codeSize = int.Parse(txtCodeSize.Text); // строк кода
+
+            EffortSize.Text = string.Format("{0} {1}", Calculation.GetEfforts(codeSize, type).ToString("F2"), "люд/міс.");
+
+            Time.Text = string.Format("{0} {1}", Calculation.GetTimeToDevelop(codeSize, type).ToString("F2"), "міс.");
+
+            DevNumber.Text = string.Format("{0} {1}", Calculation.GetPersonsToDevelop(codeSize, type).ToString(), "люд.");
+        }
+
+        private void txtCodeSize_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            isDigit = ((e.Key < Key.D0 || e.Key > Key.D9) && (e.Key != Key.Back));
+            e.Handled = isDigit;
+        }
+
+
+        //int result = comboBoxProjectType.SelectedIndex;
+
+        //EffortSize.Text = result.ToString();
+    }
+}
+
